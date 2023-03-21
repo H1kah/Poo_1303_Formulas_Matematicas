140e8b7849c1e23449b59374b2a4ece497898951
H1karu <124624556+H1kah@users.noreply.github.com>
2023-03-20 22:04:42 -0300

    Add files via upload

---
+++ MegaMan.cpp
@@ -0,0 +1,26 @@
+//Bibliotecas. Um dicionário para entender o que está sendo programado
+#include <iostream>
+using namespace std;
+//está iniciando 
+int main() {
+    //Int é para dizer que será um número inteiro
+    int life;
+    //Float é para números quebrados
+    float energy;
+    //Bool é um código para duas opções: true e false.
+    bool status;
+    //string é para escrita
+    string character;
+    //valores de cada coisa. No caso, toda a ecrita citada anteriormente será subistituída por elas
+    life = 5;
+    energy = 99.99f;
+    status = true;
+    character = "Mega Man";
+    //cout é o que vai mostrar na tela quando rodar cada coisa
+    cout<<life<<endl;
+    cout<<energy<<endl;
+    cout<<status<<endl;
+    cout<<character<<endl;
+    
+    return 0;
+}

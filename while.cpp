dea974a33db1636c462c2acc432b010055d0933f
H1karu <124624556+H1kah@users.noreply.github.com>
2023-03-27 20:14:04 -0300

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    do {
+        cout << "Digite um número entre 1 e 10: ";
+        cin >> num;
+    } while (num < 1 || num > 10);
+    
+        cout << "Você digitou o número " << num << '.';
+        
+return 0;
+}

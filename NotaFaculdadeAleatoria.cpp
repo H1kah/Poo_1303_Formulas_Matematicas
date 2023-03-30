85804fd04b0bb96ac60bd1c78326a9813d3308ee
H1karu <124624556+H1kah@users.noreply.github.com>
2023-03-30 13:27:06 -0300

    Update NotaFaculdadeAleatoria.cpp

--- NotaFaculdadeAleatoria.cpp
+++ NotaFaculdadeAleatoria.cpp
@@ -3,21 +3,36 @@
 using namespace std;
 //está iniciando 
 int main() {
+    //doble seria um float, mas com o dobro de precisão, uma versão 
     double nota1, nota2, nota3, nota4;
+    //o texto que aparecerá na interface
     cout << "Digite sua primeira nota do semestre >>>";
+    //a resposta que for dada, será o valor que vai ser colocado como nota1
     cin >> nota1;
+    //o texto que aparecerá na interface
     cout << "Digite sua segunda nota do semestre >>>";
+    //o valor será colocado como nota2
     cin >> nota2;
+    //o texto que aparecerá na interface
     cout << "Digite sua terceira nota do semestre >>>";
+    //o valor será colocado como nota3
     cin >> nota3;
+    //o texto que aparecerá na interface
     cout << "Digite sua quarta nota do semestre >>>";
+    //o valor será colocado como nota4
     cin >> nota4;
+    //está definindo o que é a soma, sendo soma = n1 + n2 + n3 + n4
     double soma = (nota1 + nota2 + nota3 + nota4);
+    //fórmula que será utilizada 
     soma = (soma/4);
+    //o texto que aparecerá na interface
     cout << "A sua média no semestre foi: " << soma << endl << endl;
+    //possibilidades. Ou seja, se a soma for >= 7 (maior ou igual)...
     if(soma >= 7){
+    //... a pessoa será aprovada, se não...
         cout << "Parabéns, você foi aprovado!" << endl;
     }
+//...será reprovada. Ou seja, else é tipo um "porém", caso a primeira resposta não se encaixe no que precisa
     else {
         cout << "Eita, você foi reprovado!" << endl;
     }

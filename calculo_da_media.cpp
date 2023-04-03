e20dbf2bf31e29857d20695f74628998dbda4783
H1karu <124624556+H1kah@users.noreply.github.com>
2023-04-03 20:32:04 -0300

    Update calculo_da_media.cpp

--- calculo_da_media.cpp
+++ calculo_da_media.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 //está iniciando 
 int main() {
+    //char está se referindo o que é o verdadeiro
+    char continuar = 's';
+    //o comando para que o comando se repita logo após o que está a seguir
+    while (continuar == 's' || continuar == 'S') {
     //doble seria um float, mas com o dobro de precisão, uma versão 
     double nota1, nota2, nota3, nota4;
     //o texto que aparecerá na interface
@@ -35,6 +39,9 @@ int main() {
 //...será reprovada. Ou seja, else é tipo um "porém", caso a primeira resposta não se encaixe no que precisa
     else {
         cout << "Eita, você foi reprovado!" << endl;
+    }
+    cout << "Deseja continuar (s/n)?" << endl;
+    cin >> continuar;
     }
     return 0;
 }

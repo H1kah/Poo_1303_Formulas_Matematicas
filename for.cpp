6a0542aaa09c92fe6127bcab30c373193abd5159
H1karu <124624556+H1kah@users.noreply.github.com>
2023-04-03 19:18:20 -0300

    Update for.cpp

--- for.cpp
+++ for.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+//biblioteca para o programa ler os códigos
 using namespace std;
 
 int main() {
-    int num;
-    
+        int num;
+    //mostrando o valor em destaque
         cout << "Digite um número inteiro positivo: ";
+    //cout mostrará o que aparecerá na interface
         cin >> num;
-    
+    //toda resposta depois da mensagem denominará o que será o num. Isso a partir do cin
         cout << "Os primeiros " << num << " numeros pares são: ";
-        
+      //for é um comando de repetição. Ou seja, de acordo com o valor que for dado, o comando se repetirá até dar falso no sistema.
         for (int i = 2; i <= num * 2; i += 2) {
             cout << i << " ";
             
